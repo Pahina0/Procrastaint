b5f0fde411dc7c9f9382564355cd4fdbbf2d8707
Alexander Wu <bunny20040628@gmail.com>
2024-10-07 22:08:04 -0700

    bump kotlin and nlp

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,6 +6,7 @@ plugins {
     alias(libs.plugins.androidLibrary).apply(false)
     alias(libs.plugins.kotlinAndroid).apply(false)
     alias(libs.plugins.kotlinMultiplatform).apply(false)
+    alias(libs.plugins.compose.compiler).apply(false)
     alias(libs.plugins.detekt)
 }
 
