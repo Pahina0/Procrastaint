1127ce94271631eb1b75250d992b0508fb6c1d64
shanhe2 <149892212+shanhe2@users.noreply.github.com>
2024-12-09 13:20:55 -0500

    Update CalendarViewScreenModel.kt
    Create list for each day object

--- androidApp/src/main/java/ap/panini/procrastaint/ui/calendar/CalendarViewScreenModel.kt
+++ androidApp/src/main/java/ap/panini/procrastaint/ui/calendar/CalendarViewScreenModel.kt
@@ -1,16 +1,63 @@
 package ap.panini.procrastaint.ui.calendar
 
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.ui.Modifier
+import androidx.core.bundle.Bundle
 import ap.panini.procrastaint.data.repositories.AppRepository
 import ap.panini.procrastaint.data.repositories.PreferenceRepository
+import ap.panini.procrastaint.ui.theme.secondaryContainerDarkHighContrast
+import cafe.adriel.voyager.core.model.StateScreenModel
 
 class CalendarViewScreenModel(
-
     private const val CALENDAR_ROWS: Int = 5,
     private const val CALENDAR_COLUMNS: Int = 7,
 
     val db: AppRepository,
     private val pref: PreferenceRepository
 ) : StateScreenModel<CalendarViewScreenModel.State>(State()) {
+
+    override fun onCreate(savedInstanceState: Bundle?){
+        super.onCreate(savedInstanceState)
+        setContent{
+            val calendarInputList by remember{
+                mutableStateOf(createCalendarList())
+            }
+            Box(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .background(secondaryContainerDarkHighContrast)
+                contentAlignment = Alignment.TopCenter
+
+            ){
+                Calendar(
+                    calendarInput = ,
+                    onDayClick = ,
+                    month = "December"
+                    modifier = Modifier
+                        .padding()
+                        .fillMaxsize()
+                        aspectratio()
+                )
+            }
+        }
+    }
+
+    private fun createCalendarList(): List<CalendarInput>{
+        val calendarInputs = mutableListOf<CalendarInput>()
+        for (i in 1..31){
+            calendarInputs.add(
+                CalendarInput(
+                    i,
+                    toDos = listOf(
+                        "Day: $i:",
+                        "2 p.m. Test Case 1",
+                        "4 p.m. Test case 2"
+                    )
+                )
+            )
+        }
+        return calendarInputs
+    }
 
     data class CalendarInput(
         val day: Int,
