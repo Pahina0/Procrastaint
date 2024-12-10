93bf078a448bef9c725a39943addf5325fc0cf88
shanhe2 <149892212+shanhe2@users.noreply.github.com>
2024-12-10 11:21:47 -0500

    Update MonthToMonthCalendar.kt
    Update and debug

--- androidApp/src/main/java/ap/panini/procrastaint/ui/calendar/MonthToMonthCalendar.kt
+++ androidApp/src/main/java/ap/panini/procrastaint/ui/calendar/MonthToMonthCalendar.kt
@@ -4,11 +4,15 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Canvas
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.geometry.CornerRadius
 import androidx.compose.ui.graphics.drawscope.Stroke
@@ -17,12 +21,34 @@ import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import ap.panini.procrastaint.ui.theme.onPrimaryContainerLight
 import ap.panini.procrastaint.ui.theme.onPrimaryLight
+import ap.panini.procrastaint.ui.theme.secondaryContainerLight
 
 class MonthToMonthCalendar : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
+            val calendarInputList by remember {
+                mutableStateOf(createCalendarList())
+            }
 
+            Box(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .background(secondaryContainerLight)
+                contentAlignment = Alignment.TopCenter
+            ) {
+                Calendar(
+                    calendarInput = calendarInputList,
+                    onDayClick =  {
+
+                    },
+                    month = "December", //apply whole year logic here needed
+                    modifier = Modifier
+                        .padding(10.dp)
+                        .fillMaxWidth()
+                        .aspectRatio(1.3f)
+                )
+            }
         }
     }
 
