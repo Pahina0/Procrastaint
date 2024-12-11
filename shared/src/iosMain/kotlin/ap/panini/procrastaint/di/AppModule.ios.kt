259a360ed7d50081a5d5d7e54f16c3001042efed
yic04 <144494441+yic04@users.noreply.github.com>
2024-12-10 19:57:36 -0500

    Update AppModule.ios.kt
    Add getDatabaseBuilder and createDataStore for iOS

--- shared/src/iosMain/kotlin/ap/panini/procrastaint/di/AppModule.ios.kt
+++ shared/src/iosMain/kotlin/ap/panini/procrastaint/di/AppModule.ios.kt
@@ -2,13 +2,45 @@ package ap.panini.procrastaint.di
 
 import androidx.datastore.core.DataStore
 import androidx.datastore.preferences.core.Preferences
+import androidx.room.Room
 import androidx.room.RoomDatabase
 import ap.panini.procrastaint.data.database.ProcrastaintDatabase
+import kotlinx.cinterop.ExperimentalForeignApi
+import platform.Foundation.NSDocumentDirectory
+import platform.Foundation.NSFileManager
+import platform.Foundation.NSURL
+import platform.Foundation.NSUserDomainMask
 
 actual fun getDatabaseBuilder(): RoomDatabase.Builder<ProcrastaintDatabase> {
-    TODO("Not yet implemented")
+    val dbFilePath = documentDirectory() + "procrastaint.db"
+    return Room.databaseBuilder<ProcrastaintDatabase>(
+        name = dbFilePath,
+    )
 }
 
+@OptIn(ExperimentalForeignApi::class)
+private fun documentDirectory(): String {
+    val documentDirectory = NSFileManager.defaultManager.URLForDirectory(
+        directory = NSDocumentDirectory,
+        inDomain = NSUserDomainMask,
+        appropriateForURL = null,
+        create = false,
+        error = null,
+    )
+    return requireNotNull(documentDirectory?.path)
+}
+
+@OptIn(ExperimentalForeignApi::class)
 actual fun createDataStore(): DataStore<Preferences> {
-    TODO("Not yet implemented")
+    return createDataStore(
+    producePath = {
+        val documentDirectory: NSURL? = NSFileManager.defaultManager.URLForDirectory(
+            directory = NSDocumentDirectory,
+            inDomain = NSUserDomainMask,
+            appropriateForURL = null,
+            create = false,
+            error = null,
+        )
+        requireNotNull(documentDirectory).path + "procrastaint.db"
+    })
 }
