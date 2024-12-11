d7594f1d5b7d992c9407319901d659e15224f92e
shanhe2 <149892212+shanhe2@users.noreply.github.com>
2024-12-10 20:38:24 -0500

    Update Calendar click animation

--- androidApp/src/main/java/ap/panini/procrastaint/ui/calendar/MonthToMonthCalendar.kt
+++ androidApp/src/main/java/ap/panini/procrastaint/ui/calendar/MonthToMonthCalendar.kt
@@ -4,8 +4,11 @@ import android.graphics.Paint
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.animation.core.animate
+import androidx.compose.animation.core.tween
 import androidx.compose.foundation.Canvas
 import androidx.compose.foundation.background
+import androidx.compose.foundation.gestures.detectTapGestures
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -14,17 +17,20 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
+import androidx.compose.runtime.*
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.geometry.CornerRadius
 import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.geometry.Size
+import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.drawscope.Stroke
 import androidx.compose.ui.graphics.nativeCanvas
+import androidx.compose.ui.graphics.Path
+import androidx.compose.ui.graphics.drawscope.clipPath
 import androidx.compose.ui.graphics.toArgb
+import androidx.compose.ui.input.pointer.pointerInput
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -32,6 +38,7 @@ import ap.panini.procrastaint.ui.theme.onPrimaryContainerLight
 import ap.panini.procrastaint.ui.theme.onPrimaryLight
 import ap.panini.procrastaint.ui.theme.onSecondaryLight
 import ap.panini.procrastaint.ui.theme.secondaryContainerLight
+import kotlinx.coroutines.launch
 
 class MonthToMonthCalendar : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -94,9 +101,18 @@ fun Calendar(
     month: String //add switch logic later to go to difference months
 ) {
 
-    var canvasSize by remember{
+    var canvasSize by remember {
+        mutableStateOf(Size.Zero)
+    }
+    var clickAnimationOffset by remember {
         mutableStateOf(Offset.Zero)
     }
+
+    var animationRadius by remember {
+        mutableStateOf(0f)
+    }
+
+    val scope = rememberCoroutineScope()
 
     Column(
         modifier = modifier,
@@ -110,12 +126,57 @@ fun Calendar(
         )
 
         Canvas(
-            modifier = Modifier.fillMaxSize()
+            modifier = Modifier
+                .fillMaxSize()
+                .pointerInput(true){
+                    detectTapGestures {
+                        //calculate the relative amount of the user click
+                        onTap = { offset ->
+                            val column = (offset.x / canvasSize.width * CALENDAR_COLUMNS.toInt() + 1)
+                            val row = (offset.x / canvasSize.height * CALENDAR_ROWS.toInt() + 1)
+                            val day = column + (row-1) * CALENDAR_COLUMNS
+                            if (day <= calendarInput.size){
+                                onDayClick(day)
+                                clickAnimationOffset = offset
+                                scope.launch {
+                                    animate(0f, 225f, animationSpec = tween(300)){value, _ ->
+                                        animationRadius = value
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
         ){
             val canvasHeight = size.height
             val canvasWidth = size.width
+            canvasSize = Size(canvasWidth, canvasHeight)
             val ySteps = canvasHeight/ CALENDAR_ROWS
             val xSteps = canvasWidth/ CALENDAR_COLUMNS
+
+            val column = (clickAnimationOffset.x / canvasSize.width * CALENDAR_COLUMNS.toInt() + 1)
+            val row = (clickAnimationOffset.x / canvasSize.height * CALENDAR_ROWS.toInt() + 1)
+
+            //path exactly the grid item
+            val path = Path().apply{
+                moveTo( (column-1)*xSteps, (row-1)*ySteps)
+                lineTo(column*xSteps, (row-1)*ySteps)
+                lineTo(column*xSteps,row*ySteps)
+                lineTo((column-1)*xSteps,row*ySteps)
+                close()
+            }
+
+            clipPath(path){
+                drawCircle(
+                    brush = Brush.radialGradiaent(
+                        listOf(onPrimaryContainerLight.copy(0.8f),onPrimaryContainerLight.copy(0.2f)),
+                        center = clickAnimationOffset,
+                        radius = animationRadius + 0.1f
+                    ),
+                    radius = animationRadius + 0.1f,
+                    center = clickAnimationOffset
+                )
+            }
 
             drawRoundRect(
                 onPrimaryContainerLight,
