ec41a8f0ff2136fbd040f88213349b59455fa739
yic04 <144494441+yic04@users.noreply.github.com>
2024-12-10 19:59:08 -0500

    Try to fix error
    kotlin.UninitializedPropertyAccessException: lateinit property cleanFilenames has not been initialized

---
+++ shared/src/iosMain/kotlin/database/ProcrastaintDatabase_Impl.kt
@@ -0,0 +1,117 @@
+package ap.panini.procrastaint.`data`.database
+
+import androidx.room.InvalidationTracker
+import androidx.room.RoomOpenDelegate
+import androidx.room.migration.AutoMigrationSpec
+import androidx.room.migration.Migration
+import androidx.room.util.TableInfo
+import androidx.room.util.TableInfo.Companion.read
+import androidx.room.util.dropFtsSyncTriggers
+import androidx.sqlite.SQLiteConnection
+import androidx.sqlite.execSQL
+import ap.panini.procrastaint.`data`.database.dao.TaskDao
+import ap.panini.procrastaint.`data`.database.dao.TaskDao_Impl
+import javax.`annotation`.processing.Generated
+import kotlin.Lazy
+import kotlin.String
+import kotlin.Suppress
+import kotlin.collections.List
+import kotlin.collections.Map
+import kotlin.collections.MutableList
+import kotlin.collections.MutableMap
+import kotlin.collections.MutableSet
+import kotlin.collections.Set
+import kotlin.collections.mutableListOf
+import kotlin.collections.mutableMapOf
+import kotlin.collections.mutableSetOf
+import kotlin.reflect.KClass
+
+@Generated(value = ["androidx.room.RoomProcessor"])
+@Suppress(names = ["UNCHECKED_CAST", "DEPRECATION", "REDUNDANT_PROJECTION", "REMOVAL"])
+public class ProcrastaintDatabase_Impl : ProcrastaintDatabase() {
+  private val _taskDao: Lazy<TaskDao> = lazy {
+    TaskDao_Impl(this)
+  }
+
+
+  protected override fun createOpenDelegate(): RoomOpenDelegate {
+    val _openDelegate: RoomOpenDelegate = object : RoomOpenDelegate(1,
+        "ba519ffe295c03163b5731a3502fe4e4", "2cb18e17c9ea316b51ba99587ec41306") {
+      public override fun createAllTables(connection: SQLiteConnection) {
+        connection.execSQL("CREATE TABLE IF NOT EXISTS `Task` (`title` TEXT NOT NULL, `description` TEXT, `startTime` INTEGER, `endTime` INTEGER, `repeatTag` TEXT, `repeatOften` INTEGER, `completed` INTEGER, `id` INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL)")
+        connection.execSQL("CREATE TABLE IF NOT EXISTS room_master_table (id INTEGER PRIMARY KEY,identity_hash TEXT)")
+        connection.execSQL("INSERT OR REPLACE INTO room_master_table (id,identity_hash) VALUES(42, 'ba519ffe295c03163b5731a3502fe4e4')")
+      }
+
+      public override fun dropAllTables(connection: SQLiteConnection) {
+        connection.execSQL("DROP TABLE IF EXISTS `Task`")
+      }
+
+      public override fun onCreate(connection: SQLiteConnection) {
+      }
+
+      public override fun onOpen(connection: SQLiteConnection) {
+        internalInitInvalidationTracker(connection)
+      }
+
+      public override fun onPreMigrate(connection: SQLiteConnection) {
+        dropFtsSyncTriggers(connection)
+      }
+
+      public override fun onPostMigrate(connection: SQLiteConnection) {
+      }
+
+      public override fun onValidateSchema(connection: SQLiteConnection):
+          RoomOpenDelegate.ValidationResult {
+        val _columnsTask: MutableMap<String, TableInfo.Column> = mutableMapOf()
+        _columnsTask.put("title", TableInfo.Column("title", "TEXT", true, 0, null,
+            TableInfo.CREATED_FROM_ENTITY))
+        _columnsTask.put("description", TableInfo.Column("description", "TEXT", false, 0, null,
+            TableInfo.CREATED_FROM_ENTITY))
+        _columnsTask.put("startTime", TableInfo.Column("startTime", "INTEGER", false, 0, null,
+            TableInfo.CREATED_FROM_ENTITY))
+        _columnsTask.put("endTime", TableInfo.Column("endTime", "INTEGER", false, 0, null,
+            TableInfo.CREATED_FROM_ENTITY))
+        _columnsTask.put("repeatTag", TableInfo.Column("repeatTag", "TEXT", false, 0, null,
+            TableInfo.CREATED_FROM_ENTITY))
+        _columnsTask.put("repeatOften", TableInfo.Column("repeatOften", "INTEGER", false, 0, null,
+            TableInfo.CREATED_FROM_ENTITY))
+        _columnsTask.put("completed", TableInfo.Column("completed", "INTEGER", false, 0, null,
+            TableInfo.CREATED_FROM_ENTITY))
+        _columnsTask.put("id", TableInfo.Column("id", "INTEGER", true, 1, null,
+            TableInfo.CREATED_FROM_ENTITY))
+        val _foreignKeysTask: MutableSet<TableInfo.ForeignKey> = mutableSetOf()
+        val _indicesTask: MutableSet<TableInfo.Index> = mutableSetOf()
+        val _infoTask: TableInfo = TableInfo("Task", _columnsTask, _foreignKeysTask, _indicesTask)
+        val _existingTask: TableInfo = read(connection, "Task")
+        if (!_infoTask.equals(_existingTask)) {
+          return RoomOpenDelegate.ValidationResult(false, """
+              |Task(ap.panini.procrastaint.data.model.Task).
+              | Expected:
+              |""".trimMargin() + _infoTask + """
+              |
+              | Found:
+              |""".trimMargin() + _existingTask)
+        }
+        return RoomOpenDelegate.ValidationResult(true, null)
+      }
+    }
+    return _openDelegate
+  }
+
+  protected override fun createInvalidationTracker(): InvalidationTracker {
+    val _shadowTablesMap: MutableMap<String, String> = mutableMapOf()
+    val _viewTables: MutableMap<String, Set<String>> = mutableMapOf()
+    return InvalidationTracker(this, _shadowTablesMap, _viewTables, "Task")
+  }
+
+  public override fun clearAllTables() {
+    super.performClear(false, "Task")
+  }
+
+  protected override fun getRequiredTypeConverterClasses(): Map<KClass<*>, List<KClass<*>>> {
+    val _typeConvertersMap: MutableMap<KClass<*>, List<KClass<*>>> = mutableMapOf()
+    _typeConvertersMap.put(TaskDao::class, TaskDao_Impl.getRequiredConverters())
+    return _typeConvertersMap
+  }
+
